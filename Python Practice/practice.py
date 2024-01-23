c5ae649f9c8ddf247610b2096e9b04e564f1e0fe
tarun5161 <156853517+tarun5161@users.noreply.github.com>
2024-01-23 13:52:42 -0800

    Delete Python Practice/practice.py

--- Python Practice/practice.py
+++
@@ -1,8 +0,0 @@
-weight = int(input("weight:"))
-unit = input("(L)bs or (K)g:")
-if unit.upper() == "L":
-    converted = weight * 0.45
-    print(f"your {converted} kilos")
-else:
-    converted = weight / 0.45
-    print(f"your {converted} pounds")
